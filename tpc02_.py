3a17335fe988cb7324b1a766be65c5f047a856a4
Matías Zanini <matias.zanini@outlook.com>
2020-05-11 20:37:45 -0300

    Inicializo el script del TPC 2

---
+++ tpc02_.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+                **Introducción a Redes Complejas en Biología de Sistemas**
+                        Trabajo Computacional 2 - Estructura a Gran Escala 
+Entrega 14/05
+
+
+Grupo: Camila Sanz, Matías Zanini.
+"""
+
+################################################################################
+#                                 PAQUETES 
+################################################################################
+
+import pandas as pd       #DB
+import numpy as np        #matemática, simil maltab 
+import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn3
+import igraph as ig
+import random
+import math
+# Evitar acumulación de mensajes de warning en el display
+import warnings  
+warnings.filterwarnings("ignore")
+
+
+#%%
+
+################################################################################
+#                               PUNTO 1 
+################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+################################################################################
+#                               PUNTO 2 
+################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+################################################################################
+#                               PUNTO 3 
+################################################################################

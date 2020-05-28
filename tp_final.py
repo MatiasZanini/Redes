34fea1fe5b73b74ed1388715a5ef3572c6f40c12
Matías Zanini <matias.zanini@outlook.com>
2020-05-28 05:56:21 -0300

    Ya puede scrapear nombre del post y likeadores
    falta scrapear la fecha

--- tp_final.py
+++ tp_final.py
@@ -4,18 +4,147 @@ Created on Wed May 27 00:36:30 2020
 
 @author: Mati
 """
-
-import requests
+import os
+from datetime import datetime
+#import requests
 from bs4 import BeautifulSoup as bs
 import numpy as np
 import matplotlib.pyplot as plt
 
+#%%
+
+'''
+Funciones
+'''
+
+def nombre_post(file, h_post, n_post):
+    
+    h = file.find(h_post)
+    
+    n = file.find(n_post, h)
+    
+    nlen = len(n_post)
+    
+    n_i = n + nlen + 1 # Inicio del nombre
+    
+    n_f = file.find('"', n_i)
+
+    return file[n_i : n_f]
+
+
+def nombre_reacts(file, h_reacts, n_reacts):
+    
+    reacters = []
+    
+    nlen = len(n_reacts)
+    
+    h = file.find(h_reacts)
+    
+    h_stop = file.find('Nuevo mensaje', h)
+    
+    stop = h_stop - nlen -1
+    
+    n = file.find(n_reacts, h)
+    
+    while n < stop:
+    
+        n_i = n + nlen + 1 # Inicio del nombre
+    
+        n_f = file.find('"', n_i) # Fin del nombre
+
+        nombre = file[n_i : n_f]
+        
+        if nombre not in ['Agregar', 'Seguir', 'Mensaje']:
+
+            reacters.append(nombre)
+            
+            print(nombre)
+            
+        n = file.find(n_reacts, n_f + 1)
+
+    return reacters
+
+
+
+
+
 
 #%%
 
-path = r'C:\Users\Mati\Desktop\prueba_1.html'
+path = 'C:/Users/Mati/Documents/GitHub/Redes/datos/'
 
-a = bs(open(path), 'html')
+filename = 'fuente_completa_likes_1.html'
+
+date_file = os.path.getctime(path+filename) # Objeto con la fecha y hora de la obtención de los datos. 
+#Atributos: .year, .month, .day, .minute, .second
+
+'''
+Tags:
+'''
+
+h_post = '<div class="sjgh65i0 l9j0dhe7 k4urcfbm du4w35lb">' # Head para encontrar al posteador
+
+n_post = '<a aria-label=' # Head para el nombre del posteador
+
+h_date = '<div class="qzhwtbm6 knvmm38d">' # Head para encontrar la fecha del post
+
+n_date = '<span id="jsc_c' # Head para el valor de la fecha
+
+h_reacts = '<div class="a8s20v7p k5wvi7nf buofh1pr pfnyh3mw l9j0dhe7 du4w35lb"><div data' # Head para encontrar los que reaccionan
+
+n_reacts = 'aria-label=' # Head para cada nombre de los que reaccionan. Ignorar "Agregar", "Seguir", "Mensaje"
+# Cortar en "Nuevo mensaje" 
+
+
+file = str(bs(open(path+filename, encoding="utf8"), 'html.parser'))
+    
+poster = nombre_post(file, h_post, n_post)
+
+reacters = nombre_reacts(file, h_reacts, n_reacts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+path = r'C:\Users\Mati\Documents\GitHub\Redes\datos\fuente_completa_likes_1.html'
+
+a = bs(open(path, encoding="utf8"), 'html')
 
 soup = bs(a.content,'html')
 

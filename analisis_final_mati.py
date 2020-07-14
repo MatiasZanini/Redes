886121870f4944f9a1b8405bda0a2a235764d630
Matías Zanini <matias.zanini@outlook.com>
2020-07-13 22:37:56 -0300

    Agregadas funciones para proyectar

--- analisis_final_mati.py
+++ analisis_final_mati.py
@@ -110,11 +110,70 @@ nx.write_gml(red_completa, save_path_red+'red_completa.gml')
 #ANALISIS
 ############
 
+save_path_red = 'C:/Users/Mati/Documents/GitHub/Redes/datos/redes_gml/'
+
+red_name = 'red_completa.gml'
+
+red_completa = nx.read_gml(save_path_red + red_name)
 
 
-# Mirar pagina: https://networkx.github.io/documentation/stable/reference/algorithms/bipartite.html en la parte
-# weightened projections y mirar la que habla del ratio de vecinos compartidos. Ver si eso sirve para establecer una
-#tolerancia.
+def nodo_conectado(red, nodo1, nodo2):
+    
+    return nodo1 in red.neighbors(nodo2) # Devuelve True si estan conectados o False sino
+
+
+def proyect_sin_posters(red):
+    
+    proyectada = nx.Graph()
+    
+    enfermitos, posts = nx.bipartite.sets(red)
+    
+    for post in posts:
+        
+        for reacter in red.neighbors(post):
+            
+            proyectada.add_node(reacter, bipartite=0)
+            
+            proyectada.add_node(post, bipartite=1)
+            
+            proyectada.add_edge(reacter, post)
+    
+    # si 2 enfermitos le dieron like a n o mas posts iguales --> enlace
+    
+    return proyectada
+    
+
+def proyect_enfermitos(red, tol):
+    
+    enfermitos, posts = nx.bipartite.sets(red)
+    
+    red_enfermita = bipartite.weighted_projected_graph(red, enfermitos)
+    
+    for enlace in red_enfermita.edges():
+        
+        nodo1, nodo2 = enlace
+        
+        if red_enfermita.get_edge_data(nodo1, nodo2)['weight'] < tol:
+            
+            red_enfermita.remove_edge(nodo1, nodo2)
+            
+    return red_enfermita
+            
+            
+#NOTA: esta dando una cantidad enorme de enlaces. Revisar
+    
+    
+    
+    
+
+'''
+vamos a proyectar primero a una red de reaccionadores, luego sobre esa red hacemos la proyeccion pesada y usamos el 
+peso de cada enlace (cantidad de vecinos compartidos) como parametro a colocarle una tolerancia.
+
+Despues podemos usar la proyeccion de la red dirigida sobre los enfermitos para encontrar (nuevamente mediante el peso)
+la cantidad de enfermitos que likearon varios posts de un posteador.
+'''
+
 
 
 
@@ -137,7 +196,18 @@ o hacer un promedio d
 hacer proyeccion incluyendo posters y ver si hay enfermitos que atraen muchos likes (enfermitos "famosos")
 
 
+# Mirar pagina: https://networkx.github.io/documentation/stable/reference/algorithms/bipartite.html en la parte
+# weightened projections y mirar la que habla del ratio de vecinos compartidos. Ver si eso sirve para establecer una
+#tolerancia.
 
+2) RED DE POPULARIDAD ENFERMITA:
+    
+    una proyeccion en la que se añade un enlace entre un posteador y un reacter si el reacter le dio like a n o mas posts
+    suyos. (podria ser dirigida de posteador a reacter)
+    
+    Pintar a los enfermitos con la categoria que le toco en el analisis de comunidades y ver si hay homofilia (enfermitos
+   de igual categoria tienen a estar juntos)
+    
 '''
 
 
